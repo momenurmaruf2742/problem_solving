fd9e20f56481e4932c14bc81524e015a9de0a31d
unknown <momenur.maruf@gmail.com>
2022-10-08 17:50:06 +0600

    codechef problem solving

--- code_chef/ltime.py
+++
@@ -1,7 +0,0 @@
-for _ in range(int(input())):
-    x,y,z = map(int(),input().split())
-    count=0
-        for i in range(x):
-            if i%3==0:
-                count +=1
-        
---
+++ code_chef/ticketfine.py
@@ -0,0 +1,3 @@
+for _ in range (int(input())):
+    x,p,q=map(int,input().split())
+    print((p-q)*x)
