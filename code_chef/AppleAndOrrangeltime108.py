6b8f0999e7e5683c2b2ff2d9f754ffa32f1920e3
unknown <momenur.maruf@gmail.com>
2022-10-08 15:48:55 +0600

    solved codechef problem

---
+++ code_chef/AppleAndOrrangeltime108.py
@@ -0,0 +1,6 @@
+x=int(input())
+a,b=map(int,input().split())
+if a+b>x:
+    print("NO")
+else:
+    print("YES")
---
+++ code_chef/LunchTime107.py
@@ -0,0 +1,6 @@
+for _ in range(int(input())):
+    x=int(input())
+    if x>=1 and x<=4:
+        print("YES")
+    else:
+        print("NO")
