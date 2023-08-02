5654a259dc343ca0eb1bcaa758e835fdf58b0eba
marufbysl <momenur.islam@byslglobal.com>
2023-08-02 19:00:33 +0600

    leetcode

---
+++ leetCode/136. Single Number.py
@@ -0,0 +1,11 @@
+from typing import List
+from collections import Counter
+class Solution:
+    
+    def singleNumber(self,nums: List[int]) -> int:
+        ls=Counter(nums)
+        for key in ls:
+            if ls[key]==1:
+                return key
+a=Solution
+print(a.singleNumber(None,[1,1,2,3,3,4,4,4]))
