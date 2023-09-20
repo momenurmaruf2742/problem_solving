19872e7a558006f6dea4c5ffdadaf383345e333e
marufbysl <momenur.islam@byslglobal.com>
2023-09-20 18:27:17 +0600

    leet code

--- leetCode/643. Maximum Average Subarray I.py
+++ leetCode/643. Maximum Average Subarray I.py
@@ -3,9 +3,9 @@ from typing import List
 
 class Solution:
     def findMaxAverage(self, nums: List[int], k: int) -> float:
-        result = [x for x in nums if x > 4 or abs(x) > 4]
+        result = [x for x in nums if x >= k or abs(x) >= k]
         result = sum(result)/k
         print(result)
         
 a=Solution
-print(a.findMaxAverage(None,[1,12,-5,-6,50,3],4))
+print(a.findMaxAverage(None,[-1],1))
