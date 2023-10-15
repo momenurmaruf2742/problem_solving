efb6e33605886270853c4319e4f51027378750d1
marufbysl <momenur.islam@byslglobal.com>
2023-10-15 13:01:48 +0600

    database leet code

---
+++ leetCode/570. Managers with at Least 5 Direct Reports.sql
@@ -0,0 +1,11 @@
+select m.name from Employee e inner join Employee m on e.managerId = m.id group by e.managerId HAVING COUNT(e.id) >= 5;
+
+
+-- Create table If Not Exists Employee (id int, name varchar(255), department varchar(255), managerId int)
+-- Truncate table Employee
+-- insert into Employee (id, name, department, managerId) values ('101', 'John', 'A', 'None')
+-- insert into Employee (id, name, department, managerId) values ('102', 'Dan', 'A', '101')
+-- insert into Employee (id, name, department, managerId) values ('103', 'James', 'A', '101')
+-- insert into Employee (id, name, department, managerId) values ('104', 'Amy', 'A', '101')
+-- insert into Employee (id, name, department, managerId) values ('105', 'Anne', 'A', '101')
+-- insert into Employee (id, name, department, managerId) values ('106', 'Ron', 'B', '101')
