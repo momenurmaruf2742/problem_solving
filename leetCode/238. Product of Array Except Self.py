347e9f1b4276b22ec0d809f13b59e05a82de3840
momenurmaruf2742 <momenur.maruf@gmail.com>
2024-02-14 21:16:23 +0600

    leetcode238

---
+++ leetCode/238. Product of Array Except Self.py
@@ -0,0 +1,15 @@
+from typing import List
+
+
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        for i in List:
+
+
+        pass
+
+        return
+
+
+c = Solution
+print(c.productExceptSelf(None,[145,665,66]))
