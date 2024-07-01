8b02aa377b56df17d1f0ab2179b2c133f114ab89
marufbysl <momenur.islam@byslglobal.com>
2024-07-01 15:17:52 +0600

    leetcode

---
+++ leetCode/189. Rotate Array.py
@@ -0,0 +1,33 @@
+from typing import List
+
+
+class Solution:
+    def reverse(self, nums, i, j):
+        li = i
+        ri = j
+
+        while li < ri:
+            temp = nums[li]
+            nums[li] = nums[ri]
+            nums[ri] = temp
+
+            li += 1
+            ri -= 1
+
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        k = k % len(nums)
+        if k < 0:
+            k += len(nums)
+        print(len(nums) - 1)
+        self.reverse(nums, 0, len(nums) - k - 1)
+        self.reverse(nums, len(nums) - k, len(nums) - 1)
+        self.reverse(nums, 0, len(nums) - 1)
+
+        return nums
+
+
+sol = Solution()
+print(sol.rotate([1, 1, 2, 3], 2))
